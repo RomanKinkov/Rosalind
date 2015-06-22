36fd7fd93c24d5b4eead9f76fe90c1aec93126f4
RomanKinkov <klaus_meine@mail.ru>
2015-06-22 21:51:16 +0400

    Create CountingDNANucleotides.py

---
+++ CountingDNANucleotides.py
@@ -0,0 +1,9 @@
+with open("d:\data\input.txt") as file:
+    line = file.readline()
+    a_count = line.count("A")
+    c_count = line.count("C")
+    g_count = line.count("G")
+    t_count = line.count("T")
+
+with open("d:\data\output.txt", "w") as file:
+    file.write(str(a_count) + " " + str(c_count) + " " + str(g_count) + " " + str(t_count))
